cc92b0c3046cb10da8f11ac86b41c59f2f6d2143
nik3348 <nicklai88@live.com>
2020-04-13 21:16:07 +0800

    Added endpoint and config files - Got fetching data from backend to work

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+export const host = 'http://localhost';
+export const port = ':1323';
+export const namespace = '/api/users';
+
+export const url = host + port + namespace;
--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import { url } from '../../config/env';
+import { getAll } from '../../api/Endpoints.json';
 
 export default function Users() {
-  const data = [{
-    id: 1, fname: 'Mark', lname: 'Otta', username: '@mdo',
-  }];
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [errors, setErrors] = useState(null);
+  const [data, setData] = useState([]);
 
   const row = data.map((datum) => (
     <tr>
       <td>{datum.id}</td>
-      <td>{datum.fname}</td>
-      <td>{datum.lname}</td>
+      <td>{datum.name}</td>
+      <td>{datum.email}</td>
       <td>{datum.username}</td>
     </tr>
   ));
+
+  useEffect(() => {
+    fetch(
+      url + getAll,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setErrors(error);
+        },
+      );
+  }, []);
 
   return (
     <div style={{ margin: 10 }}>
